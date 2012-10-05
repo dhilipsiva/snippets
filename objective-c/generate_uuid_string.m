aae3a50f83319201de90cece0cf9bf07dcf00b60
DhilipSiva Bijju <dhilipsiva@gmail.com>
2012-10-05 17:49:11 +0530

    Script to generate a UUID String #2

---
+++ objective-c/generate_uuid_string.m
@@ -0,0 +1,9 @@
+- (NSString *)uuidString {
+    // Returns a UUID
+    
+    CFUUIDRef uuid = CFUUIDCreate(kCFAllocatorDefault);
+    NSString *uuidStr = (__bridge_transfer NSString *)CFUUIDCreateString(kCFAllocatorDefault, uuid);
+    CFRelease(uuid);
+    
+    return uuidStr;
+}
