e44e318d62399c96d50ad15b14c84878b3bc2faf
DhilipSiva Bijju <dhilipsiva@gmail.com>
2013-02-11 12:18:18 +0530

    Code cleanup

--- objective-c/generate_uuid_string.m
+++
@@ -1,9 +0,0 @@
-- (NSString *)uuidString {
-    // Returns a UUID
-    
-    CFUUIDRef uuid = CFUUIDCreate(kCFAllocatorDefault);
-    NSString *uuidStr = (__bridge_transfer NSString *)CFUUIDCreateString(kCFAllocatorDefault, uuid);
-    CFRelease(uuid);
-    
-    return uuidStr;
-}
--- objective-c/get_current_screen_bounds.m
+++
@@ -1,2 +0,0 @@
-CGRect scr = [[UIScreen mainScreen] bounds];
-NSLog(@"x = %f\ny = %f\nwidth = %f\nheight = %f\n",scr.origin.x, scr.origin.y, scr.size.width, scr.size.height);
