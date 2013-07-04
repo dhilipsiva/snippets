5158dfa9e584222d7d3c64b4853a64d171cbe9be
dhilipsiva <dhilipsiva@gmail.com>
2013-07-04 08:38:04 +0530

    Backup #4
    Signed-off-by: dhilipsiva <dhilipsiva@gmail.com>

--- backup.sh
+++ backup.sh
@@ -1,25 +1,25 @@
 #! /bin/bash
 
 # Get environment variables
-. ~/.bash_profile
+. ~/.bash_profile &&\
 
-echo 'copying bash_profile...'
-cp -f ~/.bash_profile ./
+echo 'copying bash_profile...' &&\
+cp -f ~/.bash_profile ./ &&\
 
-echo 'copying gitconfig...'
-cp -f ~/.gitconfig ./
+echo 'copying gitconfig...' &&\
+cp -f ~/.gitconfig ./ &&\
 
-echo 'copying vimrc...'
-cp -f ~/.vimrc ./
+echo 'copying vimrc...' &&\
+cp -f ~/.vimrc ./ &&\
 
-echo 'copying gitignore_global...'
-cp -f ~/.gitignore_global ./
+echo 'copying gitignore_global...' &&\
+cp -f ~/.gitignore_global ./ &&\
 
-echo 'adding files to git...'
-git add --all .
+echo 'adding files to git...' &&\
+@add &&\
 
-echo 'commiting changes if any...'
-@commit "Backup #4"
+echo 'commiting changes if any...' &&\
+@commit "Backup #4" &&\
 
-echo 'syncing with remote'
+echo 'syncing with remote' &&\
 syn
