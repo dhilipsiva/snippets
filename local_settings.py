94ac516022b14d9e3510f497c19abbf674c3ecdb
dhilipsiva <dhilipsiva@gmail.com>
2013-07-31 19:35:31 +0530

    Adding local settings for django #4

---
+++ local_settings.py
@@ -0,0 +1,14 @@
+DEBUG = True
+
+BASE_URL = 'http://localhost:8000'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'app_name.sqlite',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        }
+    }
